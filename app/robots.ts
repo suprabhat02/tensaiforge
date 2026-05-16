4cb5fca9cb026bff2c3fc0fbe82c38aef72ed98f
suprabhat.supz <supz@topsoe.com>
2026-05-16 16:12:34 +0530

    Initial commit

--- app/robots.ts
+++ app/robots.ts
@@ -1,8 +1,6 @@
 import type { MetadataRoute } from "next";
 import { SITE } from "@/lib/constants";
 
-export const dynamic = "force-static";
-
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
